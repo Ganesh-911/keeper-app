b5b7ab859c11e12a643f27c3b41287c17925d8ef
student BVCE <ganeshgundubogula911@gmail.com>
2026-05-18 18:36:26 +0530

    Built keeper app using React

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,40 @@
+import React,{useState} from "react";
+
+function CreateArea(props) {
+  const [note,setNote] = useState({
+    title:"",
+    content:""
+  });
+  function handleChange(event){
+    const {name,value} = event.target;
+    setNote(prevNote =>{
+      return {
+        ...prevNote,
+        [name]:value
+      }
+    });
+  }
+  function submitNote(event){
+    event.preventDefault();
+    if(note.title.trim() === "" || note.content.trim() === ""){
+      alert("Please fill out both the title and content fields.");
+      return;
+    }
+    setNote({
+    title:"",
+    content:""
+  });
+    props.onAdd(note);
+  }
+  return (
+    <div>
+      <form>
+        <input name="title" onChange ={handleChange} value={note.title} placeholder="Title" />
+        <textarea name="content" onChange ={handleChange} value={note.content} placeholder="Take a note..." rows="3" />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
